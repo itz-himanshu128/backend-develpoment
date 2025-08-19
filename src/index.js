6a4b62a737cc86ae5257eeba7d6aeb2ac3f78a12
itz-himanshu128 <himanshu87701@gmail.com>
2025-08-19 16:24:48 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,44 @@
+// we need to add envirronmental variable to this file because as soon as file is loded we want to load our environment variables is avalable to every file 
+
+// require ('dotenv').config({path: './env'})   
+// this breaks the consistency of code so that is why there is another process to write it 
+import dotenv from 'dotenv'
+
+import connectDB from './db/index' 
+
+// in this second aporach we we write code in any other file then in index file we only import the function and execute.
+
+dotenv.config({
+    path: './env'
+})
+
+
+
+
+
+
+// here we use an aproach where we type all our code in index file and then use it.
+
+/*
+import mongoose from "mongoose";
+import {DB_NAME}from "./constants";
+import express from "express";
+const app = express()
+( async ()=> {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error" ,(error)=>{
+            console.log("APP IS NOT ABLE RUN ", error);
+            throw error 
+            app.listen(process.env.PORT,()=>{
+                console.log(`App is listening on port ${process.env.PORT}`);
+            })
+        })
+    } catch (error) {
+        console.error("ERROR: ",error)
+        throw err
+    }
+    
+ })()
+
+*/
