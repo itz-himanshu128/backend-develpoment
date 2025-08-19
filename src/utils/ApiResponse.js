ebce228ddce35eee241dafca64a5ac5f173d0c8d
itz-himanshu128 <himanshu87701@gmail.com>
2025-08-19 23:23:04 +0530

    i wrote Api's code (ApiError,ApiResponse) files

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,13 @@
+class ApiResponse{
+    constructor(statusCode,data,message="Success"){
+        this.statusCode=statusCode,
+        this.data=data,
+        this.message=message,
+        this.scccess=statusCode<400 
+        // info response (100-199)
+        // success response (200-299)
+        // redirection message (300- 399)
+        // client error message (400-499)
+        // server error message (500-599)
+    }
+}
