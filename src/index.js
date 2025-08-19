866621ee7857b90de8e4dfa6aeab139b30b9566e
itz-himanshu128 <himanshu87701@gmail.com>
2025-08-19 22:01:49 +0530

    connecting database

--- src/index.js
+++ src/index.js
@@ -12,6 +12,38 @@ dotenv.config({
     path: './env'
 })
 
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT||8000 ,()=>{
+        console.log(`Server is running at : ${process.env.PORT }`);
+    })
+    // may cause error
+    app.on("error" ,(error)=>{
+            console.log("APP IS NOT ABLE RUN ", error);
+            throw error 
+        })
+    // up to this 
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed ",err)
+})
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
